13001fe32ff650e657c7bccaf29391765f7b53a0
Victor Doros <dorosvictor@gmail.com>
2025-01-09 07:38:16 +0200

    run on multiple environments

--- cypress/models/environment.ts
+++ cypress/models/environment.ts
@@ -2,11 +2,11 @@ export default class Environment {
   getEnvironment() {
     let env = Cypress.env("env")
     if (env === "dev") {
-      return "https://todo.qacart.com/"
+      return Cypress.config("baseUrl", "https://todo.qacart.com/")
     } else if (env === "staging") {
-      return "https://todo.qacart.com/"
+      return Cypress.config("baseUrl", "https://todo.qacart.com/")
     } else if (env === "prod") {
-      return "https://todo.qacart.com/"
+      return Cypress.config("baseUrl", "https://todo.qacart.com/")
     }
   }
 }
